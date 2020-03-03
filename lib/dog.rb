31835318b6426ca1414b9869622a901c7d3ca1d9
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-03 23:58:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id=nil, name, breed)
+  def initialize(id = nil, name, breed)
     @id = id
     @name = name
     @breed = breed
@@ -19,6 +19,7 @@ class Dog
     SQL
 
     DB[:conn].execute(sql)
+    Dog.new
   end
 
   def self.drop_table
