4f66ab049e1167531f58decf42ab0bba51c5c547
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-03-04 00:11:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,8 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id:"nil", name:, breed:)
-    @id = id
+  def initialize(id:, name:, breed:)
+    @id = "nil"
     @name = name
     @breed = breed
   end
